a93582f6b3e3ef1635c85a6a66d31b81d2a5dd7c
rubi <rubi0624@nate.com>
2022-10-25 14:50:39 +0900

    layout 완료

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -2,50 +2,241 @@
   <div id="app">
     <div class="container">
       <h1>VSkeletonLoader Demo</h1>
-      <div class="samples">
-        <h3>Case #1</h3>
-        <VSkeletonLoader :content="[
-          ['box', 'title + text']
-        ]"></VSkeletonLoader>
-        <h3>Case #2</h3>
-        <VSkeletonLoader :content="[
-          ['box:9/16', 'title + text:2']
-        ]"></VSkeletonLoader>
-        <h3>Case #3</h3>
-        <div style="max-width: 50%;">
-          <VSkeletonLoader :content="[
-            ['box:16/9'],
-            ['title'],
-            ['text:2']
-          ]"></VSkeletonLoader>
+      <div>
+        <div id="textInput">
+          <span class="txt"
+            >ex&#41; <br />
+            &#91;"box", "title + text"&#93;, &#91;"text"&#93; <br />
+            &#91;"rec9_16", "title + text:3"&#93;, &#91;"text:3"&#93;,
+            &#91;"text:3"&#93;
+          </span>
+          <input
+            class="input"
+            type="text"
+            v-model="textInput"
+            v-on:keyup.enter="submitValue"
+            placeholder="타입을 입력해주세요"
+          />
+
+          <!-- <p>{{ textInput }}</p> -->
         </div>
+      </div>
+      <div class="samples">
+        <h3>Test Case</h3>
+        <VSkeletonLoader :content="submitData" />
+        <h3>Case #1</h3>
+        <VSkeletonLoader :content="type1" />
+        <h3>Case #2</h3>
+        <VSkeletonLoader :content="type2" />
+        <h3>Case #3</h3>
+        <VSkeletonLoader :content="type3" />
+        <h3>Case #4</h3>
+        <VSkeletonLoader :content="type4" />
+        <h3>Case #5</h3>
+        <VSkeletonLoader :content="type5" />
+        <h3>Case #6</h3>
+        <VSkeletonLoader :content="type6" />
+        <h3>Case #7</h3>
+        <VSkeletonLoader :content="type7" />
+        <h3>Case #8</h3>
+        <VSkeletonLoader :content="type8" />
+        <h3>Case #9</h3>
+        <VSkeletonLoader :content="type9" />
+        <h3>Case #10</h3>
+        <VSkeletonLoader :content="type10" />
+        <h3>Case #11</h3>
+        <VSkeletonLoader :content="type11" />
+        <h3>Case #12</h3>
+        <VSkeletonLoader :content="type12" />
+        <h3>Case #13</h3>
+        <VSkeletonLoader :content="type13" />
+        <h3>Case #14</h3>
+        <VSkeletonLoader :content="type14" />
+        <h3>Case #15</h3>
+        <VSkeletonLoader :content="type15" />
+        <h3>Case #16</h3>
+        <VSkeletonLoader :content="type16" />
+        <h3>Case #17</h3>
+        <VSkeletonLoader :content="type17" />
+        <h3>Case #18</h3>
+        <VSkeletonLoader :content="type18" />
+        <h3>Case #19</h3>
+        <VSkeletonLoader :content="type19" />
+        <h3>Case #20</h3>
+        <VSkeletonLoader :content="type20" />
+        <h3>Case #21</h3>
+        <VSkeletonLoader :content="type21" />
+        <h3>Case #22</h3>
+        <VSkeletonLoader :content="type22" />
+        <h3>Case #23</h3>
+        <VSkeletonLoader :content="type23" />
+        <h3>Case #24</h3>
+        <VSkeletonLoader :content="type24" />
+        <h3>Case #25</h3>
+        <VSkeletonLoader :content="type25" />
+        <h3>Case #26</h3>
+        <VSkeletonLoader :content="type26" />
+        <h3>Case #27</h3>
+        <VSkeletonLoader :content="type27" />
+        <h3>Case #28</h3>
+        <VSkeletonLoader :content="type28" />
+        <h3>Case #29</h3>
+        <VSkeletonLoader :content="type29" />
+        <h3>Case #30</h3>
+        <VSkeletonLoader :content="type30" />
+        <h3>Case #31</h3>
+        <VSkeletonLoader :content="type31" />
+        <h3>Case #32</h3>
+        <VSkeletonLoader :content="type32" />
+        <h3>Case #33</h3>
+        <VSkeletonLoader :content="type33" />
+        <h3>Case #34</h3>
+        <VSkeletonLoader :content="type34" />
+        <h3>Case #35</h3>
+        <VSkeletonLoader :content="type35" />
+        <h3>Case #36</h3>
+        <VSkeletonLoader :content="type36" />
+        <h3>Case #37</h3>
+        <VSkeletonLoader :content="type37" />
+        <h3>Case #38</h3>
+        <VSkeletonLoader :content="type38" />
+        <h3>Case #39</h3>
+        <VSkeletonLoader :content="type39" />
+        <h3>Case #40</h3>
+        <VSkeletonLoader :content="type40" />
+        <h3>Case #41</h3>
+        <VSkeletonLoader :content="type41" />
+        <h3>Case #42</h3>
+        <VSkeletonLoader :content="type42" />
+        <h3>Case #43</h3>
+        <VSkeletonLoader :content="type43" />
+        <h3>Case #44</h3>
+        <VSkeletonLoader :content="type44" />
+        <h3>Case #45</h3>
+        <VSkeletonLoader :content="type45" />
+        <h3>Case #46</h3>
+        <VSkeletonLoader :content="type46" />
+        <h3>Case #47</h3>
+        <VSkeletonLoader :content="type47" />
+        <h3>Case #48</h3>
+        <VSkeletonLoader :content="type48" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import '@/components/VSkeletonLoader'
-import VSkeletonLoader from './components/VSkeletonLoader.vue'
+import "@/components/VSkeletonLoader";
+import VSkeletonLoader from "./components/VSkeletonLoader.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    VSkeletonLoader
-  }
-}
-</script>
+    VSkeletonLoader,
+  },
+  data() {
+    return {
+      type1: [["box", "text"]],
+      type2: [["box", "text:2"]],
+      type3: [["box", "text:3"]],
+      type4: [["circle", "text"]],
+      type5: [["circle", "text:2"]],
+      type6: [["circle", "text:3"]],
+      type7: [["rec16_9", "text"]],
+      type8: [["rec16_9", "text:2"]],
+      type9: [["rec16_9", "text:3"]],
+      type10: [["rec9_16", "text"]],
+      type11: [["rec9_16", "text:2"]],
+      type12: [["rec9_16", "text:3"]],
+      type13: [["box", "title + text"]],
+      type14: [["box", "title + text:2"]],
+      type15: [["box", "title + text:3"]],
+      type16: [["circle", "title + text"]],
+      type17: [["circle", "title + text:2"]],
+      type18: [["circle", "title + text:3"]],
+      type19: [["rec16_9", "title + text"]],
+      type20: [["rec16_9", "title + text:2"]],
+      type21: [["rec16_9", "title + text:3"]],
+      type22: [["rec9_16", "title + text"]],
+      type23: [["rec9_16", "title + text:2"]],
+      type24: [["rec9_16", "title + text:3"]],
+      type25: [["box", "title + text"], ["text"]],
+      type26: [["box", "title + text:2"], ["text:2"]],
+      type27: [["box", "title + text:3"], ["text:3"]],
+      type28: [["circle", "title + text"], ["text"]],
+      type29: [["circle", "title + text:2"], ["text:2"]],
+      type30: [["circle", "title + text:3"], ["text:3"]],
+      type31: [["rec16_9", "title + text"], ["text"]],
+      type32: [["rec16_9", "title + text:2"], ["text:2"]],
+      type33: [["rec16_9", "title + text:3"], ["text:3"]],
+      type34: [["rec9_16", "title + text"], ["text"]],
+      type35: [["rec9_16", "title + text:2"], ["text:2"]],
+      type36: [["rec9_16", "title + text:3"], ["text:3"]],
+      type37: [["box", "title + text"], ["text"], ["text"]],
+      type38: [["box", "title + text:2"], ["text:2"], ["text:2"]],
+      type39: [["box", "title + text:3"], ["text:3"], ["text:3"]],
+      type40: [["circle", "title + text"], ["text"], ["text"]],
+      type41: [["circle", "title + text:2"], ["text:2"], ["text:2"]],
+      type42: [["circle", "title + text:3"], ["text:3"], ["text:3"]],
+      type43: [["rec16_9", "title + text"], ["text"], ["text"]],
+      type44: [["rec16_9", "title + text:2"], ["text:2"], ["text:2"]],
+      type45: [["rec16_9", "title + text:3"], ["text:3"], ["text:3"]],
+      type46: [["rec9_16", "title + text"], ["text"], ["text"]],
+      type47: [["rec9_16", "title + text:2"], ["text:2"], ["text:2"]],
+      type48: [["rec9_16", "title + text:3"], ["text:3"], ["text:3"]],
 
+      textInput: "",
+      submitData: [],
+    };
+  },
+  methods: {
+    convertArrStrToArr(inputStr) {
+      return inputStr
+        .replace(/"/g, "")
+        .replace(/'/g, "")
+        .replace(/\[/g, "")
+        .replace(/], /g, "-")
+        .replace(/]/g, "")
+        .split("-")
+        .map((s) => s.split(",").map((v) => v.trim()));
+    },
+
+    submitValue() {
+      try {
+        this.submitData = this.convertArrStrToArr(this.textInput);
+        this.textInput = "";
+      } catch (e) {
+        console.error(e);
+      }
+    },
+  },
+};
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  width: 800px;
+  margin: 0 auto;
 }
-.container {
-  margin: 20px;
+h1 {
+  margin: 50px 0;
 }
-.samples {
-  max-width: 720px;
+h3 {
+  margin: 30px 0;
+}
+#textInput {
+  display: flex;
+  align-items: center;
+}
+.txt {
+  display: inline-block;
+  width: 50%;
+  margin-right: 20px;
+}
+.input {
+  width: 50%;
+  height: 30px;
+  padding: 5px;
+  border-radius: 5px 10px;
+  border: 1px solid #ddd;
+  outline: none;
 }
 </style>
--- demo/src/components/VSkeletonLoader.vue
+++ demo/src/components/VSkeletonLoader.vue
@@ -1,8 +1,51 @@
 <template>
   <div class="v-skeleton-loader">
-    <div class="row" v-for="(row, rowIndex) in content" :key="rowIndex">
-      <span class="element" v-for="(element, elementIndex) in row" :key="elementIndex">
-        {{ element }}
+    <div
+      class="row"
+      v-for="(row, rowIndex) in content"
+      :key="rowIndex"
+      v-bind:class="rowIndex"
+    >
+      <span
+        class="col"
+        v-for="(element, elementIndex) in row"
+        :key="elementIndex"
+      >
+        <template v-if="element.includes('circle') || element.includes('box')">
+          <span class="element" :class="element">
+            {{ element }}
+          </span>
+        </template>
+        <template v-if="element.includes('rec16') || element.includes('rec9')">
+          <span class="element rec" :class="element">
+            {{ element }}
+          </span>
+        </template>
+        <template v-if="element.includes('title')">
+          <span class="element title">
+            {{ element }}
+          </span>
+        </template>
+        <template v-if="element === 'text'">
+          <span class="element text">
+            {{ element }}
+          </span>
+        </template>
+        <template v-if="element === 'title + text'">
+          <span class="element text">
+            {{ element }}
+          </span>
+        </template>
+        <template v-if="element.includes('text:')">
+          <span
+            class="element text"
+            :class="element"
+            v-for="(e, i) in parseInt(element.split(':')[1], 10)"
+            :key="i"
+          >
+            {{ element }}
+          </span>
+        </template>
       </span>
     </div>
   </div>
@@ -10,25 +53,105 @@
 
 <script>
 export default {
-  name: 'VSkeletonLoader',
+  name: "VSkeletonLoader",
   props: {
     content: {
-      type: Array
-    }
-  }
-}
+      type: Array,
+    },
+  },
+  //  포털 , 이중배열, 인풋 그려지게
+};
 </script>
 
 <style scoped>
+.v-skeleton-loader {
+  padding: 30px;
+  border: 3px solid #ddd;
+}
 .row {
   display: flex;
-  justify-content: space-between;
+  flex-wrap: wrap;
+  align-items: center;
+  margin: -5px;
+}
+.row:nth-child(2) {
+  margin-top: 13px;
+}
+.col {
+  margin: 0 5px;
+}
+.col:last-child {
+  flex: 1 0 auto;
 }
 .element {
-  flex: 1;
-  display: inline-block;
-  padding: 20px;
-  background-color: #ddd;
-  box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.1);
+  display: block;
+  position: relative;
+  width: 100%;
+  margin: 5px 0;
+  background-color: #dddbdd;
+  border-radius: 5px;
+  text-indent: -9999px;
+  overflow: hidden;
+}
+.element:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  transform: translateX(-100%);
+  background: linear-gradient(
+    90deg,
+    transparent,
+    rgba(255, 255, 255, 0.5),
+    transparent
+  );
+  animation: shimmer 1.5s infinite;
+}
+@keyframes shimmer {
+  100% {
+    transform: translateX(100%);
+  }
+}
+.element.box {
+  width: 70px;
+  height: 0;
+  padding-bottom: 100%;
+}
+.element.circle {
+  width: 70px;
+  height: 0;
+  padding-bottom: 100%;
+  border-radius: 50%;
+}
+.element.title {
+  height: 20px;
+}
+.element.title:first-child {
+  width: 30%;
+}
+.element.text {
+  height: 15px;
+}
+.element.text:nth-child(2) {
+  width: 70%;
+}
+.element.text:nth-child(3) {
+  width: 50%;
+}
+.element.text:nth-child(4) {
+  width: 60%;
+}
+
+.element.rec16_9 {
+  width: 140px;
+  height: 0;
+  padding-bottom: 70%;
+}
+.element.rec9_16 {
+  width: 98px;
+  height: 0;
+  padding-bottom: 143%;
 }
 </style>
