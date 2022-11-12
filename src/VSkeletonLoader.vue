2b7dfdcf6843f85d3614ef5f4631c4ed97d8f3d2
gliburch <studio.gliburch@gmail.com>
2022-11-12 23:53:01 +0900

    element 랜더 로직을 보다 단순화 한다

--- src/VSkeletonLoader.vue
+++ src/VSkeletonLoader.vue
@@ -1,168 +1,116 @@
 <template>
   <div class="v-skeleton-loader" :class="{ animation: options.animate }">
     <div
-      class="row"
-      v-for="(row, rowIndex) in $options.filters.forceNestedArray(content)"
-      :key="rowIndex"
-      v-bind:class="rowIndex"
-    >
+      v-for="(row, rowIndex) in $options.filters.forceToNestedArray(content)" :key="rowIndex"
+      class="row">
       <div
         class="col"
         v-for="(element, elementIndex) in row"
         :key="elementIndex"
         :style="{
           'flex-grow': (() => {
-            if (element.includes('title') || element.includes('text')) {
-              return 1;
-            } else {
+            if (element.indexOf('box') > -1 || element.indexOf('circle') > -1) {
               return 0;
+            } else {
+              return 1;
             }
-          })(),
-        }"
-      >
-        <span
-          v-if="element.includes('title')"
-          class="element title"
-          :style="{
-            height: options.defaultSizes.title + 'px',
-            borderRadius: options.radius + 'px',
-            backgroundColor: options.primaryColor,
-            '--secondaryColor': options.secondaryColor,
-            '--speed': options.speed,
-          }"
-        ></span>
-        <template v-if="element.includes('text:')">
-          <span
-            v-for="(element, i) in parseInt(element.split(':')[1], 10)"
-            class="element text"
-            :class="element"
-            :key="i"
-            :style="{
-              height: options.defaultSizes.text + 'px',
-              borderRadius: options.radius + 'px',
-              backgroundColor: options.primaryColor,
-              '--secondaryColor': options.secondaryColor,
-              '--speed': options.speed,
-            }"
-          >
+          })()
+        }">
+        <template v-if="element === 'blank'">
+          <span class="elements">
+            <span class="element blank"></span>
           </span>
         </template>
-        <template v-else-if="element.includes('text')">
-          <span
-            v-for="(element, i) in (element[0], 1)"
-            class="element text"
-            :key="i"
-            :style="{
-              height: options.defaultSizes.text + 'px',
-              borderRadius: options.radius + 'px',
-              backgroundColor: options.primaryColor,
-              '--secondaryColor': options.secondaryColor,
-              '--speed': options.speed,
-            }"
-          ></span>
+        <template v-if="element.includes('title')">
+          <span class="elements">
+            <span
+              class="element title"
+              :style="{
+                height: options.defaultSizes.title + 'px',
+                borderRadius: options.radius + 'px',
+                backgroundColor: options.primaryColor,
+                '--secondaryColor': options.secondaryColor,
+                '--speed': options.speed,
+              }">
+            </span>
+          </span>
         </template>
-        <template v-if="element === 'box'">
-          <span
-            v-for="(element, i) in (element[0], 1)"
-            class="element"
-            :class="element"
-            :key="i"
-            :style="{
-              width: options.defaultSizes.box + 'px',
-              height: options.defaultSizes.box + 'px',
-              borderRadius: options.radius + 'px',
-              backgroundColor: options.primaryColor,
-              '--secondaryColor': options.secondaryColor,
-              '--speed': options.speed,
-            }"
-          ></span>
+        <template v-if="element.includes('text')">
+          <span class="elements">
+            <span
+              v-for="(elementChild, i) in parseTextChildren(element)" :key="i"
+              class="element text"
+              :style="{
+                height: options.defaultSizes.text + 'px',
+                borderRadius: options.radius + 'px',
+                backgroundColor: options.primaryColor,
+                '--secondaryColor': options.secondaryColor,
+                '--speed': options.speed,
+              }">
+            </span>
+          </span>
         </template>
-        <span
-          v-if="element.includes('box')"
-          class="element"
-          :class="element"
-          :style="{
-            width: (() => {
-              if (!element.includes('/')) {
-                return element.split(':')[1] + 'px';
-              }
-              if (element.includes('/')) {
-                return element.split(':')[1].split('/')[0] + 'px';
-              }
-              return options.defaultSizes.box + 'px';
-            })(),
-            height: (() => {
-              if (!element.includes('/')) {
-                return element.split(':')[1] + 'px';
-              }
-              if (element.includes('/')) {
-                return element.split(':')[1].split('/')[1] + 'px';
-              }
-              return options.defaultSizes.box + 'px';
-            })(),
-            borderRadius: options.radius + 'px',
-            backgroundColor: options.primaryColor,
-            '--secondaryColor': options.secondaryColor,
-            '--speed': options.speed,
-          }"
-        >
-        </span>
-        <template v-if="element === 'circle'">
-          <span
-            v-for="(element, i) in (element[0], 1)"
-            class="element circle"
-            :class="element"
-            :key="i"
-            :style="{
-              width: options.defaultSizes.circle + 'px',
-              height: options.defaultSizes.circle + 'px',
-              borderRadius: options.circleRadius + '%',
-              backgroundColor: options.primaryColor,
-              '--secondaryColor': options.secondaryColor,
-              '--speed': options.speed,
-            }"
-          ></span>
+        <template v-if="element.includes('box')">
+          <span class="elements">
+            <span
+              class="element box"
+              :style="{
+                width: (() => {
+                  if (!parseBoxSize(element).includes('/')) {
+                    return parseBoxSize(element).split(':')[1] + 'px';
+                  }
+                  if (parseBoxSize(element).includes('/')) {
+                    return parseBoxSize(element).split(':')[1].split('/')[0] + 'px';
+                  }
+                  return options.defaultSizes.box + 'px';
+                })(),
+                height: (() => {
+                  if (!parseBoxSize(element).includes('/')) {
+                    return parseBoxSize(element).split(':')[1] + 'px';
+                  }
+                  if (parseBoxSize(element).includes('/')) {
+                    return parseBoxSize(element).split(':')[1].split('/')[1] + 'px';
+                  }
+                  return options.defaultSizes.box + 'px';
+                })(),
+                borderRadius: options.radius + 'px',
+                backgroundColor: options.primaryColor,
+                '--secondaryColor': options.secondaryColor,
+                '--speed': options.speed,
+              }">
+            </span>
+          </span>
         </template>
-        <span
-          v-if="element.includes('circle:')"
-          class="element circle"
-          :class="element"
-          :style="{
-            width: (() => {
-              if (!element.includes('/')) {
-                return element.split(':')[1] + 'px';
-              }
-              if (element.includes('/')) {
-                return element.split(':')[1].split('/')[0] + 'px';
-              }
-              return options.defaultSizes.circle + 'px';
-            })(),
-            height: (() => {
-              if (!element.includes('/')) {
-                return element.split(':')[1] + 'px';
-              }
-              if (element.includes('/')) {
-                return element.split(':')[1].split('/')[1] + 'px';
-              }
-              return options.defaultSizes.circle + 'px';
-            })(),
-            borderRadius: options.circleRadius + '%',
-            backgroundColor: options.primaryColor,
-            '--secondaryColor': options.secondaryColor,
-            '--speed': options.speed,
-          }"
-        >
-        </span>
-        <template v-if="element === 'blank'">
-          <span
-            v-for="(element, i) in (element[0], 1)"
-            class="element"
-            :key="i"
-            :style="{
-              width: options.defaultSizes.box + 'px',
-              height: options.defaultSizes.box + 'px',
-            }"
-          ></span>
+        <template v-if="element.includes('circle')">
+          <span class="elements">
+            <span
+              class="element circle"
+              :style="{
+                width: (() => {
+                  if (!parseBoxSize(element).includes('/')) {
+                    return parseBoxSize(element).split(':')[1] + 'px';
+                  }
+                  if (parseBoxSize(element).includes('/')) {
+                    return parseBoxSize(element).split(':')[1].split('/')[0] + 'px';
+                  }
+                  return options.defaultSizes.circle + 'px';
+                })(),
+                height: (() => {
+                  if (!parseBoxSize(element).includes('/')) {
+                    return parseBoxSize(element).split(':')[1] + 'px';
+                  }
+                  if (parseBoxSize(element).includes('/')) {
+                    return parseBoxSize(element).split(':')[1].split('/')[1] + 'px';
+                  }
+                  return options.defaultSizes.circle + 'px';
+                })(),
+                borderRadius: options.circleRadius + '%',
+                backgroundColor: options.primaryColor,
+                '--secondaryColor': options.secondaryColor,
+                '--speed': options.speed,
+              }">
+            </span>
+          </span>
         </template>
       </div>
     </div>
@@ -171,85 +119,117 @@
 
 <script>
 export default {
-  name: "VSkeletonLoader",
+  name: 'VSkeletonLoader',
   props: {
     content: {
       type: Array,
+      default: function () {
+        return ['box', 'text:2'];
+      }
     },
     options: {
       type: Object,
       default: function () {
         return {
           radius: 3,
-          primaryColor: "#ddd",
-          secondaryColor: "rgba(255, 255, 255, 0.5)",
+          primaryColor: '#ddd',
+          secondaryColor: 'rgba(255, 255, 255, 0.5)',
           animate: true,
-          speed: "1.5s",
+          speed: '1.5s',
           defaultSizes: {
             box: 50,
             circle: 50,
             title: 13,
             text: 10,
-          },
-        };
-      },
-    },
+          }
+        }
+      }
+    }
   },
   filters: {
-    forceNestedArray: function (arr) {
+    forceToNestedArray: function (arr) {
+      // MARK: ['box', 'text'] 와 깉이 1차원 배열이 입력되어도, 구조를 통일시키기 위해 [['box', 'text']] 의 2차원 배열로 바꿉니다.
       if (Array.isArray(arr) && !Array.isArray(arr[0])) {
         return [arr];
       } else {
         return arr;
       }
-    },
+    }
   },
-};
+  methods: {
+    parseTextChildren: function (element) {
+      if (element.includes(':')) {
+        const elementName = element.split(':')[0];
+        const count = element.split(':')[1];
+        let elementChildren = [];
+        for (let i = 0; i < count; i++) {
+          elementChildren.push(elementName);
+        }
+        return elementChildren;
+      } else {
+        return element;
+      }
+    },
+    parseBoxSize: function (element) {
+      if (element.includes(':')) {
+        return element;
+      } else {
+        return element + ':' + this.options.defaultSizes.box;
+      }
+    },
+    parseCircleSize: function (element) {
+      if (element.includes(':')) {
+        return element;
+      } else {
+        return element + ':' + this.options.defaultSizes.circle;
+      }
+    }
+  }
+}
 </script>
 
 <style lang="scss">
 .v-skeleton-loader {
-
   .row {
     display: flex;
     flex-wrap: wrap;
     align-items: center;
-    margin: -5px;
-
+    margin: 0 -10px;
+    & + .row {
+      margin-top: 15px;
+    }
     .col {
       flex-grow: 1;
-      margin: 0 5px;
+      margin: 0 10px;
+      .elements {
+        display: block;
+        & + .elements {
+          margin-top: 10px;
+        }
+      }
     }
   }
-
 }
 
 .element {
   display: block;
   overflow: hidden;
   position: relative;
-  margin: 5px 0;
-  text-indent: -9999px;
-  overflow: hidden;
-
+  & + .element {
+    margin-top: 5px;
+  }
   &.circle {
     border-radius: 50%;
   }
-
   &.title {
-    width: 15%;
+    width: 30%;
+    min-width: 100px;
   }
-
-  &.text:nth-child(2) {
-    width: 70%;
-  }
-
-  &.text:nth-child(3) {
-    width: 50%;
-  }
-
-  &.text:nth-child(4) {
-    width: 60%;
+  &.text {
+    width: 100%;
+    &:last-child {
+      width: 70%;
+    }
   }
 }
 
