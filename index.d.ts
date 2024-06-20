07978989d5fca3a383dfa69fbee63b58ea220853
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2024-06-20 11:17:31 +0900

    fix: options 파라미터 optional로 변경, prop용 타입정의 추가

--- index.d.ts
+++ index.d.ts
@@ -5,50 +5,50 @@ declare module 'vue-json-skeleton-loader' {
    * @param options
    * @param options.componentName default: 'VSkeletonLoader'
    */
-  export function install(app: App, options: { componentName?: string }): void
-}
+  export function install(app: App, options?: { componentName?: string }): void
 
-export type ContentType = Array<string> | Array<Array<string>>
+  // export type ContentType = Array<string> | Array<Array<string>>
 
-export type OptionsType = {
-  /**
-   * @default 3
-   */
-  radius?: number
-  /**
-   * @default '#dddddd'
-   */
-  primaryColor?: string
-  /**
-   * @default 'rgba(255, 255, 255, 0.5)'
-   */
-  secondaryColor?: string
-  /**
-   * @default true
-   */
-  animate?: boolean
-  /**
-   * @default '1.5s'
-   */
-  speed?: string
-  defaultSizes?: DefaultSizesType
-}
+  // export type OptionsType = {
+  //   /**
+  //    * @default 3
+  //    */
+  //   radius?: number
+  //   /**
+  //    * @default '#dddddd'
+  //    */
+  //   primaryColor?: string
+  //   /**
+  //    * @default 'rgba(255, 255, 255, 0.5)'
+  //    */
+  //   secondaryColor?: string
+  //   /**
+  //    * @default true
+  //    */
+  //   animate?: boolean
+  //   /**
+  //    * @default '1.5s'
+  //    */
+  //   speed?: string
+  //   defaultSizes?: DefaultSizesType
+  // }
 
-type DefaultSizesType = {
-  /**
-   * @default 50
-   */
-  box: number
-  /**
-   * @default 50
-   */
-  circle: number
-  /**
-   * @default 20
-   */
-  title: number
-  /**
-   * @default 10
-   */
-  text: number
+  // type DefaultSizesType = {
+  //   /**
+  //    * @default 50
+  //    */
+  //   box: number
+  //   /**
+  //    * @default 50
+  //    */
+  //   circle: number
+  //   /**
+  //    * @default 20
+  //    */
+  //   title: number
+  //   /**
+  //    * @default 10
+  //    */
+  //   text: number
+  // }
 }
